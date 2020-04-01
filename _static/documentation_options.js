c9fef4fa30f44d9cbe1723e1f06a26fadb10b08e
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-04-01 18:22:46 +0000

    Update docs after building Travis build 1330 of edgi-govdata-archiving/web-monitoring-processing
    The docs were built from the branch 'master' against the commit
    8052a9e93c1c58afd0682be6c4b7f7abab7de7cf.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/edgi-govdata-archiving/web-monitoring-processing/jobs/669825565.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.7.1/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.50.g62377ea',
+    VERSION: '0+untagged.50.g8052a9e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
