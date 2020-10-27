7aeeac71bcfbc5810771bace822424a334f7f4b8
Rob Brackett <rob@robbrackett.com>
2020-10-27 10:55:26 -0700

    Add deprecation notices for diffs/diff-server
    Fixes #638.

--- web_monitoring/diff/diff_errors.py
+++ web_monitoring/diff/diff_errors.py
@@ -1,3 +1,14 @@
+# -------- DEPRECATED -----------
+#
+# This package has been deprecated and will be removed soon. Everything in it
+# is now part of web-monitoring-diff.
+#
+#   https://github.com/edgi-govdata-archiving/web-monitoring-diff
+#   https://pypi.org/project/web-monitoring-diff/
+#
+# See also: https://github.com/edgi-govdata-archiving/web-monitoring-processing/issues/638
+#
+
 class UndiffableContentError(ValueError):
     """
     Raised when the content provided to a differ is incompatible with the
