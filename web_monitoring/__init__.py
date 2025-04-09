5f352a0aced73f45c54df103c20e92000451f90d
Rob Brackett <rob@robbrackett.com>
2025-04-08 19:21:47 -0700

    Make pyflakes happy with new version stuff

--- web_monitoring/__init__.py
+++ web_monitoring/__init__.py
@@ -1,6 +1,10 @@
 import logging
 import os
-from ._version import __version__, __version_tuple__ # noqa: F401
+from ._version import __version__, __version_tuple__  # noqa: F401
+
+# Satisfy pyflakes
+assert __version__
+assert __version_tuple__
 
 
 if os.environ.get('LOG_LEVEL'):
